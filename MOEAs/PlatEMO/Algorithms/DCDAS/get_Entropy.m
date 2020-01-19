bb3ed45225e8c9a020d3ef51eaba02c191c8303f
freddieyang <yf970113@outlook.com>
2020-01-19 20:13:57 +0800

    Update the get_Entropy

--- MOEAs/PlatEMO/Algorithms/DCDAS/get_Entropy.m
+++ MOEAs/PlatEMO/Algorithms/DCDAS/get_Entropy.m
@@ -1,27 +1,3 @@
-% function entropy = get_Entropy(Population)
-% % GET_ENTROPY 此处显示有关此函数的摘要
-% %   此处显示详细说明
-%     entropy = 0;
-%     objs = Population.objs;
-%     [K,M] = size(objs);
-%     f_min = min(objs);
-%     f_max = max(objs);
-%     C = floor(K*(objs - f_min)./(f_max - f_min));
-%     entropy = zeros(1,M);
-%     for m = 1:M
-%         ent = 0;
-%         for k = 1:K
-%             cell = sum(ismember(C(:,m),k));
-%             p = cell/(K);
-%             if p~=0
-%                 ent = ent+p*log(p);
-%             end
-%         end
-%         entropy(m) = -ent;
-%     end
-%     entropy = entropy/log(K);
-% end
-
 function entropy = get_Entropy(Population)
 %GET_ENTROPY 此处显示有关此函数的摘要
 %   此处显示详细说明
